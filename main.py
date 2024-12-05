ea453d7752b49103b51687e19bf283c2bd020650
Laura-2115 <laura.iriskinian@laplateforme.io>
2024-12-05 14:01:42 +0100

    Mise à jour

--- main.py
+++ main.py
@@ -61,47 +61,26 @@ board = create_board()
 print_board(board)
 
 def verif(board, player):
-    if board[0][0]== player and board [0][1]==player and board[0][2]== player: #ligne 1
+    if board[0][0]== player and board [0][1]==player and board[0][2]== player or \
+        board[1][0] == player and board [1][1] == player and board[1][2]== player or \
+        board[2][0] == player and board [2][1] == player and board[2][2]== player or \
+        board[0][0] == player and board [1][0] == player and board[2][0]== player or \
+        board[0][1] == player and board [1][1] == player and board[2][1]== player or \
+        board[0][2] == player and board [1][2] == player and board[2][2]== player or \
+        board[0][0] == player and board [1][1] == player and board[2][2]== player or \
+        board[0][2] == player and board [1][1] == player and board[2][0]== player:
         print (f"Player {player} Gagne")
         winner = True
         return winner
-    elif board[1][0] == player and board [1][1] == player and board[1][2]== player: #ligne 2
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[2][0] == player and board [2][1] == player and board[2][2]== player: #ligne 3
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[0][0] == player and board [1][0] == player and board[2][0]== player: #colonne 1
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[0][1] == player and board [1][1] == player and board[2][1]== player: #colonne 2
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[0][2] == player and board [1][2] == player and board[2][2]== player: #colonne 3
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[0][0] == player and board [1][1] == player and board[2][2]== player: #diagonale de gauche à droite
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[0][2] == player and board [1][1] == player and board[2][0]== player: #diagonale de droite à gauche
-        print (f"Player {player} Gagne")
-        winner = True
-        return winner
-    elif board[0][0] != "1" and board[0][1] != "2" and board[0][2] != "3" and \
+    if board[0][0] != "1" and board[0][1] != "2" and board[0][2] != "3" and \
     board[1][0] != "4" and board[1][1] != "5" and board[1][2] != "6" and \
     board[2][0] != "7" and board[2][1] != "8" and board[2][2] != "9" :
-        print (f"match nul !")
+        print ("match nul !")
         winner = True
         return winner
     else:
         winner = False
-        return winner
+    return winner
 
 
 # exemple pour pouvoir jouer --> boucle à créer
