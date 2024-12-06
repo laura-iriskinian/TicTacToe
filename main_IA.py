39f8c2dd63f81b5aa02a4aed0d0b39fb3d8927da
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2024-12-06 12:04:22 +0100

    ajout des commentaires

--- main_IA.py
+++ main_IA.py
@@ -6,12 +6,14 @@ def create_board():
     ]
     return board
 
+
 def print_board(board):
     print(f" {board[0][0]} | {board[0][1]} | {board[0][2]}")
     print("---|---|---")
     print(f" {board[1][0]} | {board[1][1]} | {board[1][2]}")
     print("---|---|---")
     print(f" {board[2][0]} | {board[2][1]} | {board[2][2]}")
+
 
 def user_move(board, player):
     move = input("Entrez votre mouvement (1-9): ")
@@ -49,7 +51,7 @@ def user_move(board, player):
     return boelan
 
 def IA(board, player):
-    player="O"
+    
     for i in range(3):
         for j in range(3):
             if board[i][j] in "123456789":
@@ -57,9 +59,8 @@ def IA(board, player):
                 return True
     return False
 
-board = create_board()
-print_board(board)
 
+# def pour vérifier la posisition (ligne/colonne/diagonale)
 def verif(board, player):
     if board[0][0]== player and board [0][1]==player and board[0][2]== player or \
         board[1][0] == player and board [1][1] == player and board[1][2]== player or \
@@ -72,6 +73,7 @@ def verif(board, player):
         print (f"Player {player} Gagne")
         winner = True
         return winner
+    # vérifie le match nul
     if board[0][0] != "1" and board[0][1] != "2" and board[0][2] != "3" and \
     board[1][0] != "4" and board[1][1] != "5" and board[1][2] != "6" and \
     board[2][0] != "7" and board[2][1] != "8" and board[2][2] != "9" :
@@ -83,26 +85,29 @@ def verif(board, player):
     return winner
 
 
-# exemple pour pouvoir jouer --> boucle à créer
+# variable pour que la boucle commence
 winner = False
 player = "X"
+
+board = create_board()
+print_board(board)
 
 while not winner:
     print(f"Joueur: {player}")
     if player == "X":
-        boelan = user_move(board, player) #si le joueur est X, la fonction user_move s'applque
+        boelan = user_move(board, player) #si le joueur est X, la fonction user_move s'applique
     else:
-        boelan = IA(board, player) #sinon c'est la fonction auto_move qui s'applique
+        boelan = IA(board, player) #sinon c'est l'IA
     
-    if boelan == True:
-        print_board(board)
-        winner = verif(board, player)
-        if player == "X":
+    if boelan == True:                     # si mouvement joueur bolean = true 
+        print_board(board)                 # affiche le tableau
+        winner = verif(board, player)      # vérifie si victoire ou pas
+        if player == "X":                  # change de joueur
             player = "O"
         else:
             player = "X"
 
-    if winner == True:
+    if winner == True:                      #si winner propose le retry ou fin de partie
         print("Voulez vous rejouer ?")
         reset = input("Rentrez oui ou non : ")
         if reset == "oui":
